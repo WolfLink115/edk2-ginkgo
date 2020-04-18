bedb689bdb900f72a6105e557bb879b18cbb4451
loserq <>
2020-04-18 12:25:23 +0800

    add GINKGO_UEFI

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -e
 . build_common.sh
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
 GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p ginkgo/ginkgo.dsc
-gzip -c < workspace/Build/ginkgo/DEBUG_GCC5/FV/ginkgo_UEFI.fd >uefi.img
+gzip -c < workspace/Build/ginkgo/DEBUG_GCC5/FV/GINKGO_UEFI.fd >uefi.img
 cat crosshatch.dtb >>uefi.img
